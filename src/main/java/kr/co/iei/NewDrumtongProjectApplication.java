4c5b6d5ef31d81419c82fbaf6e67dca4d24a4e75
wjsgusdn96tp-create <wjsgusdn96tp@gmail.com>
2025-08-25 10:57:02 +0900

    Initial commit

---
+++ src/main/java/kr/co/iei/NewDrumtongProjectApplication.java
@@ -0,0 +1,13 @@
+package kr.co.iei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewDrumtongProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewDrumtongProjectApplication.class, args);
+	}
+
+}
