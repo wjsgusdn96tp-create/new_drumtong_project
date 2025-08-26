163f26e76ca8296350a5c0117023cf69386e1135
wjsgusdn96tp-create <wjsgusdn96tp@gmail.com>
2025-08-26 16:08:59 +0900

    map

---
+++ src/main/java/kr/co/iei/order/vo/ShopTbl.java
@@ -0,0 +1,20 @@
+package kr.co.iei.order.vo;
+
+import org.apache.ibatis.type.Alias;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Alias(value = "order")
+public class ShopTbl {
+	private String shopName;
+	private String shopAddress;
+	private String shopLatitude;
+	private String shopLongitude;
+	
+}
