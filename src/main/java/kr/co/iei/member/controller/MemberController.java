14c8baee4245cecb03bc3d66332060e6b4910f6a
seojm0629 <seojm0629@gmail.com>
2025-08-26 15:42:33 +0900

    login

--- src/main/java/kr/co/iei/member/controller/MemberController.java
+++ src/main/java/kr/co/iei/member/controller/MemberController.java
@@ -2,13 +2,29 @@ package kr.co.iei.member.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpSession;
 import kr.co.iei.member.model.service.MemberService;
+import kr.co.iei.member.model.vo.Member;
 
 @Controller
 @RequestMapping(value = "/member")
 public class MemberController {
 	@Autowired
 	private MemberService memberService;
-}
+	
+	@GetMapping(value="/loginFrm")
+	public String loginFrm() {
+		return "member/login";
+	}// loginFrm
+	
+	@PostMapping(value="/login")
+	public String login(Member m, Model model, HttpSession session) {
+		return "redirect:/";
+	}// login
+	
+}// MemberController Class
