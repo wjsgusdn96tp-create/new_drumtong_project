125858e688bf55fe40e67d2dea94a182372027b5
seojm0629 <seojm0629@gmail.com>
2025-08-29 14:43:38 +0900

    join

--- src/main/java/kr/co/iei/member/controller/MemberController.java
+++ src/main/java/kr/co/iei/member/controller/MemberController.java
@@ -37,13 +37,24 @@ public class MemberController {
 	}// joinFrm
 	
 	@PostMapping(value="/join")
-	public String join(Member m, Model model) {
-		int result = memberService.insertMember(m);
-		model.addAttribute("title", "회원가입 완료");
-		model.addAttribute("text", "가입을 환영합니다");
-		model.addAttribute("icon", "success");
-		model.addAttribute("loc", "/redirect:/");
-		return "common/msg";
+	public String join(Member m, Model model, HttpSession session) {
+	    // 1. 세션에서 인증 성공 여부 가져오기
+	    Boolean emailVerified = (Boolean) session.getAttribute("emailVerified");
+	    // 2. 인증 안됐을 경우
+	    if(emailVerified == null || !emailVerified) {
+	        model.addAttribute("title", "회원가입 실패");
+	        model.addAttribute("text", "이메일 인증을 완료해야 가입할 수 있습니다.");
+	        model.addAttribute("icon", "error");
+	        model.addAttribute("loc", "/member/joinFrm");
+	        return "common/msg";
+	    }
+	    int result = memberService.insertMember(m);
+	    model.addAttribute("title", "회원가입 완료");
+	    model.addAttribute("text", "가입을 환영합니다");
+	    model.addAttribute("icon", "success");
+	    model.addAttribute("loc", "/member/loginFrm");	    
+	    session.removeAttribute("emailVerified");
+	    return "common/msg";	    	
 	}// join
 	
 	// ajax 이메일 조회
