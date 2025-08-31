c7d03b76ac3c0877315e377827a4ea9431bca254
wjsgusdn96tp-create <wjsgusdn96tp@gmail.com>
2025-08-31 16:44:51 +0900

    hy

--- src/main/java/kr/co/iei/oreder/controller/OrderController.java
+++ src/main/java/kr/co/iei/oreder/controller/OrderController.java
@@ -42,10 +42,27 @@ public class OrderController {
 	public String orderListpage(int productNo,Model model) {
 		Product p = orderService.option(productNo);
 		
+		//productNo = DB에 물품번호 기준으로 담겨있는 정보들 이름,이미지,가격 등등
+		
 		model.addAttribute("p", p);
 		
 		return "order/Orderoption";
 	}
 
+	
+	/*
+	@GetMapping("/Orderoption")
+	public String orderListpage(int productNo, String shopName, int reqPage, Model model) {
+	    Product p = orderService.option(productNo);
+	    model.addAttribute("p", p);
+	    
+	    model.addAttribute("shopName", shopName); // 지점명 전달
+	    model.addAttribute("reqPage", reqPage);   // 페이지 번호 전달
+	    return "order/Orderoption";
+	}
+	*/
+	
+	
+	
 	
 }
