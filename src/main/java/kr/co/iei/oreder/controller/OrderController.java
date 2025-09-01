94153651018614eae5a1a8ca80c84496bbcfaa9e
wjsgusdn96tp-create <wjsgusdn96tp@gmail.com>
2025-09-01 11:16:37 +0900

    hyun

--- src/main/java/kr/co/iei/oreder/controller/OrderController.java
+++ src/main/java/kr/co/iei/oreder/controller/OrderController.java
@@ -42,18 +42,19 @@ public class OrderController {
 			@SessionAttribute(required = false) Member member) {
 		
 		Product p = orderService.option(productNo);
+		
+		//매장이름 받아오기
 		ShopTbl shop = orderService.getShop(shopName); 
 		
 		//productNo = DB에 물품번호 기준으로 담겨있는 정보들 이름,이미지,가격 등등
 		
 		model.addAttribute("p", p);
 		model.addAttribute("shop", shop);
+		
 		//로그인 하면 회원번호 나오게하기
 		 if(member != null) {
 	        model.addAttribute("memberNo", member.getMemberNo());
 	        }
-		 System.out.println("shopName = " + shopName);
-		 System.out.println("shop = " + shop);
 		 
 		return "order/Orderoption";
 	}
@@ -64,9 +65,9 @@ public class OrderController {
 	@PostMapping("/DrumtongCart")
 	@ResponseBody
 	public int insertCart(CartItem ct, @SessionAttribute(required = false) Member member) {
-		//세션에서 가져온 회원번호 세팅하기 ct에 저장 잊지말자 
 		
-		
+		//세션에서 가져온 회원번호 세팅하기 잊지말자 
+
 		ct.setMemberNo(member.getMemberNo());
 	    
 		int result = orderService.insertCart(ct); 
@@ -74,16 +75,16 @@ public class OrderController {
 	    return result; 
 	}
 	
-	//강사님한테 꼭 물어보기 할인에 대해서
-	
 	
 	// 장바구니 페이지 열기
 	@GetMapping("/DrumtongCart")
-	public String cartPage(Model model, @SessionAttribute(required = false) Member member) {
+	public String cartPage(Model model,int productNo,String shopName,String productName,
+			@SessionAttribute(required = false) Member member) {
 	    
 		int num = member.getMemberNo();
 		
-		List list = orderService.selectCartList(num);
+		List list = orderService.selectCartList(num,productNo,shopName,productName);
+		
 	    model.addAttribute("list", list);
 	    return "order/DrumtongCart";
 	}
