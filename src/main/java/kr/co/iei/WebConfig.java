e61207fe47e4965ebe226b755a3bbe000ceec2ac
hybye <hyb6019@gmail.com>
2025-09-05 10:43:37 +0900

    interceptor

--- src/main/java/kr/co/iei/WebConfig.java
+++ src/main/java/kr/co/iei/WebConfig.java
@@ -3,8 +3,12 @@ package kr.co.iei;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import kr.co.iei.util.AdminInterceptor;
+import kr.co.iei.util.LoginIntercepter;
 
 
 @Configuration
@@ -26,8 +30,29 @@ public class WebConfig implements WebMvcConfigurer {
 		
 		registry.addResourceHandler("/product/**")
 		.addResourceLocations("file:///"+root+"/product/");
+		
+		registry.addResourceHandler("/customer/**")
+		.addResourceLocations("file:///"+root+"/customer/");
 	}
 	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new LoginIntercepter())
+				.addPathPatterns(
+							"/customer/listGj",
+							"/customer/listComplain",
+							"/customer/listOpinion",
+							"/customer/writeFrm",
+							"/customer/view",
+							"/customer/updateFrm"
+						)
+				.excludePathPatterns(
+							"/customer/list"
+							
+						);
+		registry.addInterceptor(new AdminInterceptor())
+				.addPathPatterns("/admin/**");
+	}
 	
 	
 	
